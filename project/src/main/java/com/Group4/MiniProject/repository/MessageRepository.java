632b6d373563b8ab2422362c9d46adc3b98ea8fd
Mirim <lll123kong@naver.com>
2025-10-22 11:23:39 +0900

    [feat] #6 - MessageRepository 메세지 개별 조회  구현

--- project/src/main/java/com/Group4/MiniProject/repository/MessageRepository.java
+++ project/src/main/java/com/Group4/MiniProject/repository/MessageRepository.java
@@ -3,8 +3,13 @@ package com.Group4.MiniProject.repository;
 import com.Group4.MiniProject.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
     List<Message> findByReceivedUserId(Long userId);
+    
+    // 메세지 내용 확인
+    Optional<Message> findByIdAndReceivedUserId(UUID id, Long userId);
 }
+
