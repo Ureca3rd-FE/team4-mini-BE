93267e07d7574184369b0612c58e8f029fe6efe1
hyunwoo <hyunwoo@hyunwooui-MacBookAir.local>
2025-10-22 10:25:29 +0900

    [feat] UserRepositry, Service 구현

--- project/src/main/java/com/Group4/MiniProject/service/UserService.java
+++ project/src/main/java/com/Group4/MiniProject/service/UserService.java
@@ -1,21 +1,35 @@
 package com.Group4.MiniProject.service;
 
 import com.Group4.MiniProject.dto.UserRequestDto;
+import com.Group4.MiniProject.entity.User;
+import com.Group4.MiniProject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
     public void signup(UserRequestDto requestDto) {
         if (requestDto.getNickname() == null || requestDto.getNickname().trim().isEmpty()) {
             throw new IllegalArgumentException("닉네임을 입력해주세요.");
         }
-        if (requestDto.getPassword() == null || requestDto.getPassword().length() < 4) {
-            throw new IllegalArgumentException("비밀번호는 최소 4자 이상이어야 합니다.");
+        if (requestDto.getPassword() == null || requestDto.getPassword().length() > 4) {
+            throw new IllegalArgumentException("비밀번호는 4자리 이하여야 합니다.");
         }
         if (!requestDto.getPassword().matches("\\d{4}")) {
             throw new IllegalArgumentException("비밀번호는 숫자 4자리로 구성되어야 합니다.");
         }
-        // TODO: 실제 회원가입 로직 구현
+        if(userRepository.existsByNickname(requestDto.getNickname())) {
+            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+        }
+
+        User user = User.builder()
+                .nickname(requestDto.getNickname())
+                .password(requestDto.getPassword())
+                .build();
+        userRepository.save(user);
     }
+
 }
